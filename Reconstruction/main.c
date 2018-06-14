8d3f021b268ea62b91e1f7d0a3381ba5eb82edd5
root <root@localhost.localdomain>
2018-06-14 21:46:41 +0000

    Traitement d'un fichier data

--- Reconstruction/main.c
+++ Reconstruction/main.c
@@ -15,38 +15,69 @@ void VMatrix_init(Vertex** vertexMatrix, int NB_layers, int NB_points_niv);
 void VMatrix_free(Vertex** vertexMatrix, int NB_layers, int NB_points_niv);
 void VMatrix_disp(Vertex** vertexMatrix, int NB_layers, int NB_points_niv);
 void VMatrix_to_stl (Vertex** VMat, int NB_layers, int NB_points_niv);
+int readPrecision();
+void VMatrix_init_final(Vertex** vertexMatrix, int NB_layers, int NB_points_niv);
 //######################################################################################
 //##################################      MAIN      ####################################
 //######################################################################################
-int main(int argc , char* argv[]){
-	
-	if( argc != 2 ) {
-      		printf("Usage avec un entier (1-10): ./test <precision>\n");
-		return 0;
-   	}
+int main(){
+
 //SEED INIT FOR RANDOM
 	srand(time(NULL));
 
-//CONVERTING INPUT PRECISION FROM STRING TO INT
-	
-	char* ptr = NULL;
-   	long ret;
-   	ret = strtol(argv[1], &ptr, 10);
-		
-	
-	precision = (int) ret;    					// 1 MAX .....10 MIN 
-	int NB_layers = RES_Z_AXIS/precision; 	
-	int NB_points_niv = RAD_RES_MAX/precision ;
+//CONVERTING INPUT PRECISION FROM STRING TO INT [VERSION 1.0]
+//	
+//	char* ptr = NULL;
+//   	long ret;
+//   	ret = strtol(argv[1], &ptr, 10);
+//	precision = (int) ret;    					// 1 MAX .....10 MIN 
+//
+
+//AQUISITION FROM FILE
+
+//LECTURE DE LA PRECISION
+int precision=readPrecision();		
+printf("Precision =%d\n",precision);
+
+
+
+
+//	int NB_layers = RES_Z_AXIS/precision; 	
+//	int NB_points_niv = RAD_RES_MAX/precision ;
+int NB_layers =3; 	
+int NB_points_niv = 9 ;
 		
 //CREATION DE LA MATRICE REPRESENTANT UN NUAGE DE POINT ACQUIS
 	Vertex** grille = VMatrix_allocate(NB_layers, NB_points_niv);
-	VMatrix_init(grille, NB_layers,NB_points_niv);
+	VMatrix_init_final(grille, NB_layers,NB_points_niv);
 	
 	VMatrix_to_stl (grille, NB_layers, NB_points_niv);
 
 
 	VMatrix_free (grille, NB_layers,NB_points_niv);
 	return 0;
+}
+
+//#######################################################################################
+//#######################################################################################
+//#######################################################################################
+//#######################################################################################
+//#######################################################################################
+int readPrecision(){
+	FILE* fp = fopen("data","r");
+	
+	if(fp!=NULL){
+		char buff[5];	
+		if(fgets(buff,4,fp) != NULL){
+			fclose(fp);
+			return atoi(buff);	
+		}
+		fclose(fp);
+	}
+
+	printf("Could not read precision from file. set by default to 1\n");
+	return 1;	
+	
 }
 //#######################################################################################
 Facet* FacetArray_allocate(int NB_layers, int NB_points_niv){
@@ -137,7 +168,7 @@ return vertexMatrix;
 
 
 //#######################################################################################
-//INITIALISATION
+//INITIALISATION [V1.0] TEST SEULEMENT
 void VMatrix_init(Vertex** vertexMatrix, int NB_layers, int NB_points_niv){
 
 	for(int i=0; i<NB_layers; i++){	
@@ -149,6 +180,30 @@ void VMatrix_init(Vertex** vertexMatrix, int NB_layers, int NB_points_niv){
 			(vertexMatrix[i][j]).z = i*H_FACTOR*precision ; //replace Z component with real one
 		}
 	}
+}
+//#######################################################################################
+//INITIALISATION [V2.0]
+void VMatrix_init_final(Vertex** vertexMatrix, int NB_layers, int NB_points_niv){
+	FILE* fp = fopen("data","r");
+	if(fp!=NULL){
+		float x,y,z;
+		char buff[10];
+		fgets(buff,9,fp);
+		for(int i=0; i<NB_layers; i++){	
+			for(int j=0; j<NB_points_niv; j++){	
+				//READING COORDINATES FROM TEXT FILE
+				x=atoi(fgets(buff,9,fp));
+				y=atoi(fgets(buff,9,fp));
+				z=atoi(fgets(buff,9,fp));
+
+				(vertexMatrix[i][j]).x = x;
+				(vertexMatrix[i][j]).y = y;
+				(vertexMatrix[i][j]).z = z;
+			}
+		}
+
+	}
+
 }
 
 //#######################################################################################
